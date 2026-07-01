03689c9445760029547f0c15eb07c24927598c83
stivenaluguel-cyber <stiven.aluguel@gmail.com>
2026-07-01 14:39:53 -0300

    Corrigir: remover tag </a> órfã após LeadCaptureButton em mar-positano
    Removed 'Baixar Catálogo' link from the page.

--- src/app/empreendimento/fontana/mar-positano-centro-balneario-rincao-sc/page.tsx
+++ src/app/empreendimento/fontana/mar-positano-centro-balneario-rincao-sc/page.tsx
@@ -202,8 +202,6 @@ export default function Page() {
             </div>
             <div style={{ display:'flex', alignItems:'center', justifyContent:'flex-end' }}>
               <LeadCaptureButton slug="mar-positano-centro-balneario-rincao-sc" construtora_slug="fontana" className="mp-cta" />
-                Baixar Catálogo
-              </a>
             </div>
           </div>
           <div style={{ display:'grid', gridTemplateColumns:'repeat(4,1fr)', gap:16 }}>
